987fbd69b5164fc40940b5d14ff14ecdae05922f
RobinNorling <robin_norling@outlook.com>
2018-10-13 12:50:39 +0200

    derr

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ app.get('/', function(req, res,next) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(4200);
+app.get('/resources/:resource_type/:resource_name', function(req, res) {
+	res.sendFile(__dirname + '/resources/' + req.params.resource_type + '/' + req.params.resource_name);
+});
 
-io.on('connection', function(client){
+io.on('connection', function(client) {
 	console.log('Player Connected... ');
 
-	client.on('join', function(data){
+	client.on('join', function(data) {
 		console.log(data); 
 		client.emit('messages', 'Hello from server');
 	});
 });
 
-
+server.listen(4200);
